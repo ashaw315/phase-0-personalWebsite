2ca027408c7715a6e03e41941ee0092e70235f32
Adam <ashaw315@gmail.com>
2021-11-14 14:04:14 +0100

    updating for criteria

---
+++ flatiron.js
@@ -0,0 +1,9 @@
+const flatiron = document.getElementById('flatiron');
+flatiron.addEventListener('click', function() {
+alert('Hello Flatiron!');
+});
+
+const ironflat = document.getElementById('ironflat');
+ironflat.addEventListener('click', function() {
+alert('Hello Ironflat!');
+});
